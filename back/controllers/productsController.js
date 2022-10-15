be8718a7ecf3971011e71ea11f31b482fcf74cc3
ArmandoColMen <87383495+ArmandoColMen@users.noreply.github.com>
2022-10-15 17:58:36 -0500

    Conexión a MongoDB

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,21 @@
+const producto=require("../models/productosModel");
+
+//Ver la lista de productos
 exports.getProducts=(req,res,next)=>{
     res.status(200).json({
         success:true,
         messaje:'En esta ruta usted va a poder ver todos los porductos'
     })
 }
+//Ver la lista de productos
+
+
+//Crear nuevo producto /api/producto/nuevo
+exports.newProduct=async(req,res,next)=>{
+    const product=await producto.create(req.body);
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+//Crear nuevo producto /api/producto/nuevo
