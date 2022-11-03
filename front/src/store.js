535a299504e479fb2cb286110694909bf87e3acb
Lina Desalvador <lindesalvador@gmail.com>
2022-11-02 20:05:26 -0500

    Se arreglo el conflicto de la ruta ProductReducer

--- front/src/store.js
+++ front/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools} from 'redux-devtools-extension';
-import { productsReducer, productDetailsReducer } from './reducer/productReducer';
+import { productsReducer, productDetailsReducer } from './reducer/ProductReducer';
 
 const reducer= combineReducers ({
     products:productsReducer,
